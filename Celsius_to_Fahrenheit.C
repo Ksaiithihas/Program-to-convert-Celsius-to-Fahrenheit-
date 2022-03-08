9658c8055da4c63f28b75aa16a31cad1ddc972a7
Ksaiithihas <101169844+Ksaiithihas@users.noreply.github.com>
2022-03-08 13:17:02 +0530

    Create Celsius_to_Fahrenheit.C

---
+++ Celsius_to_Fahrenheit.C
@@ -0,0 +1,12 @@
+
+#include <stdio.h>
+
+void main ()
+{
+    int Celsius,Fahrenheit;
+    printf("Please Enter the Celsius :");
+    scanf("%d",&Celsius);
+    Fahrenheit=(Celsius*1.8)+32;
+    printf ("Fahrenheit is : %d",Fahrenheit);
+       
+}
